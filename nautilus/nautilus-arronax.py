ef2d0fca3930b62f3fdf1bc9c21515c28b006783
Florian Diesch <diesch@localhost>
2012-05-27 06:16:55 +0200

    * nautilus/nautilus-arronax.py   - use the path param in get_background_items() * po/*   - updated translations

--- nautilus/nautilus-arronax.py
+++ nautilus/nautilus-arronax.py
@@ -71,8 +71,8 @@ class ColumnExtension(GObject.GObject, Nautilus.MenuProvider):
         return self._create_menu_item(label, func),
 
 
-    def get_background_items(self, window, file):
-        label, func = self._create_starter_in(settings.USER_DESKTOP_DIR)
+    def get_background_items(self, window, path):
+        label, func = self._create_starter_in(path)
         return self._create_menu_item(label, func), 
 
 
