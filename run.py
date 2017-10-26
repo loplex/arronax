f1b342c3cb5755b1e93edc61cb12b81d5002028b
Florian Diesch <diesch@esprimo.daheim>
2017-10-26 21:14:06 +0200

    * Added run.py

---
+++ run.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+#
+# -*- coding: utf-8 -*-
+#
+
+import arronax
+import arronax.settings
+import os.path
+import sys
+
+fullpath = os.path.abspath(__file__)
+path = os.path.split(fullpath)[0]
+sys.path = [path] + sys.path
+
+DATA_DIR = os.path.normpath(os.path.join(path, 'data'))
+
+arronax.settings.DATA_DIR = DATA_DIR
+arronax.settings.UI_DIR = os.path.join(DATA_DIR, 'ui')
+
+arronax.main()
