baa46163d55d9c30ffeb436b75610dca862bff94
Florian Diesch <devel@florian-diesch.de>
2019-04-26 21:44:00 +0200

    Use GLib function for XDG dirs

--- arronax/xdgdirs.py
+++ arronax/xdgdirs.py
@@ -20,84 +20,24 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import os, os.path
+from gi.repository import GLib
+import os.path
 
 
-def _get_env_var(name, default=''):
-    path = os.environ.get(name, default)
-    if path == '':
-        return default
-    else:
-        return path
+XDG_DATA_HOME = GLib.get_user_data_dir()
+XDG_CONFIG_HOME = GLib.get_user_config_dir()
+XDG_CACHE_HOME = GLib.get_user_cache_dir()
+XDG_RUNTIME_DIR = GLib.get_user_runtime_dir()
+XDG_CONFIG_DIRS = GLib.get_system_config_dirs()
+XDG_DATA_DIRS = GLib.get_system_data_dirs()
 
-def _user_path(*dirs):
-    path = os.path.join('~', *dirs)
-    return os.path.expanduser(path)
-
-
-XDG_DATA_HOME   = _get_env_var('XDG_DATA_HOME',
-                               _user_path('.local', 'share'))
-XDG_CONFIG_HOME = _get_env_var('XDG_CONFIG_HOME',
-                               _user_path('.config'))
-XDG_DATA_DIRS   = _get_env_var('XDG_DATA_DIRS',
-                               ' /usr/local/share/:/usr/share/')
-XDG_CONFIG_DIRS = _get_env_var('XDG_CONFIG_DIRS',
-                               '/etc/xdg')
-XDG_CACHE_HOME  = _get_env_var('XDG_CACHE_HOME',
-                               _user_path('.cache'))
-XDG_RUNTIME_DIR = _get_env_var('XDG_RUNTIME_DIR',
-                               None)
-
-USERDIR_DEFAULTS = '/etc/xdg/user-dirs.defaults'
-USERDIR_CONFIG = os.path.join(XDG_CONFIG_HOME, 'user-dirs.dirs')
-
-def _read_user_dir_defaults(path):
-    result = {}
-    try:
-        with open (path) as input:
-            for line in input:
-                line = line.strip()
-                if not line.startswith('#'):
-                    key, value = line.split('=', 1)
-
-                    value = value.strip()
-                    value = os.path.join(os.path.expanduser('~/'), value)
-
-                    key = key.strip().lower()
-                    
-                    result[key] = value
-    except IOError:
-        pass
-    return result
-
-
-def _read_user_dirs_home(path):
-    result = {}
-    try:
-        with open (path) as input:
-            for line in input:
-                line = line.strip()
-                if line.startswith('XDG_'):
-                    key, value = line.split('=', 1)
-
-                    value = value.replace('"','').replace('$HOME', '~')
-                    value = os.path.expanduser(value)
-
-                    key = key[4:-4].lower()
-                    
-                    result[key.strip()] = value.strip()                
-    except IOError:
-        pass
-    return result
-
-
-def _get_user_dirs():
-    dirs = _read_user_dir_defaults(USERDIR_DEFAULTS)
-    dirs.update(_read_user_dirs_home(USERDIR_CONFIG))
-    return dirs
 
 def get_user_dir(name, default):
-    dirs = _get_user_dirs()
-    return dirs.get(name, default)
-
+    name = 'DIRECTORY_{}'.format(name.upper())
+    id = getattr(GLib.UserDirectory, name, None)
+    dir = GLib.get_user_special_dir(id)
+    if dir is None:
+        return os.path.expanduser(default)
+    else:
+        return dir
 
